dd760dd739e70b2bed8fbe9d2777205dc9180e9e
Karl Sebby <karl.sebby@xdbio.com>
2019-06-13 15:16:23 -0600

    Fixed paths et. al.

--- content_maps/workflow_maps.py
+++ content_maps/workflow_maps.py
@@ -2,10 +2,10 @@ from collections import OrderedDict
 from itertools import chain
 
 example_workflows = OrderedDict([
-    ('WF_1f4d8f.cb', 'workflows/example_workflows/cat_sort/1.0/cat_sort.cwl'),
-    ('WF_0b650e.e4', 'workflows/example_workflows/hello_world/1/hello_world_with_checker_workflow.cwl'),
-    ('WF_680a59.6e', 'workflows/example_workflows/md5sum_workflow/1/md5sum-workflow.cwl'),
-    ('WF_ba9852.6e', 'workflows/example_workflows/md5sum_checker/1/checker-workflow-wrapping-workflow.cwl')
+    ('WF_1f4d8f.cb', 'cwl-workflows/example_workflows/cat_sort/1.0/cat_sort.cwl'),
+    ('WF_0b650e.e4', 'cwl-workflows/example_workflows/hello_world/1/hello_world_with_checker_workflow.cwl'),
+    ('WF_680a59.6e', 'cwl-workflows/example_workflows/md5sum_workflow/1/md5sum-workflow.cwl'),
+    ('WF_ba9852.6e', 'cwl-workflows/example_workflows/md5sum_checker/1/checker-workflow-wrapping-workflow.cwl')
 ])
 
 
