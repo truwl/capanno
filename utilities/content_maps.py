11c754485f46da48adebafec69a733776376d5c8
Karl Sebby <karl.sebby@xdbio.com>
2019-08-28 13:01:47 -0600

    Generation of cwl tool content maps is working. Initiated file_management module.

--- utilities/content_maps.py
+++ utilities/content_maps.py
@@ -1,6 +1,6 @@
 
 from ruamel.yaml import safe_load, YAML
-from utilities.config import CWL_TOOL_DIR
+from utilities.config import CWL_TOOL_DIR, BASE_DIR
 from utilities.helpers.get_paths import get_cwl_tool, get_tool_inputs, get_cwl_tool_metadata, get_tool_version_dir
 
 def make_content_map():
@@ -9,7 +9,7 @@ def make_content_map():
         for version_dir in tool_dir.iterdir():
             tool_map = make_tool_map(tool_dir.name, version_dir.name)
             content_map[f"{tool_dir.name}_{version_dir.name}"] = tool_map
-    outfile_path = CWL_TOOL_DIR / 'temp' / 'content_map_test.yaml'
+    outfile_path = BASE_DIR / 'temp' / 'content_map_test.yaml'
     yaml = YAML(pure=True)
     yaml.default_flow_style = False
     yaml.indent(mapping=2, sequence=4, offset=2)
@@ -32,23 +32,33 @@ def make_tool_map(tool_name, tool_version):
         tool_map[parent_identifier] = str(tool_metadata)
         subdir_names.remove('common')
         for subdir_name in subdir_names:
-            if len(subdir_name.split('_')) > 2:
-                raise NotImplementedError(f"There are more than one underscore in directory {subdir_name}. Can't handle this yet.")
-            try:
+            subtool_name_parts = subdir_name.split('_')
+            subtool_name_parts_len = len(subtool_name_parts)
+            if subtool_name_parts_len == 2:  # The common case.
                 tool_name_from_dir, subtool_name = subdir_name.split('_')
-            except ValueError:
-                print(f"Error for {subdir_name}")
+            elif subtool_name_parts_len == 1: # have a subtool that is the 'main' part of the tool; i.e. not a submodule. e.g. md5sum and md5sum_check
+                tool_name_from_dir = subtool_name_parts[0]
+                subtool_name = None
+            else:
+                raise NotImplementedError(
+                    f"There are zero or more than one underscore in directory {subdir_name}. Can't handle this yet.")
             assert (tool_name_from_dir == tool_name), f"{tool_name} should be equal to {tool_name_from_dir}."
             subtool_cwl = get_cwl_tool(tool_name, tool_version, subtool_name=subtool_name)
             subtool_metadata = get_cwl_tool_metadata(tool_name, tool_version, subtool_name=subtool_name, parent=False)
             with subtool_metadata.open('r') as subtool_metadata_file:
                 subtool_metadata_dict = safe_load(subtool_metadata_file)
             subtool_version = subtool_metadata_dict['version']
-            try:
-                subtool_identifier = subtool_metadata_dict['identifier']
-            except KeyError:
-                print(f"No identifier field found for {tool_name} {tool_version} {subdir_name}")
+            subtool_identifier = subtool_metadata_dict['identifier']
+
             tool_map[subtool_identifier] = str(subtool_cwl)
+    else: # Not a complex tool. Should just have one directory for main tool.
+        tool_metadata = get_cwl_tool_metadata(tool_name, tool_version)
+        with tool_metadata.open('r') as metadata_file:
+            tool_metadata_dict = safe_load(metadata_file)
+        tool_identifier = tool_metadata_dict['identifier']
+        tool_cwl_version = tool_metadata_dict['version']
+
+        tool_map[tool_identifier] = str(get_cwl_tool(tool_name, tool_version))
     return tool_map
 
 
---
+++ utilities/helpers/file_management.py
@@ -0,0 +1,10 @@
+
+
+def dump_dict_to_yaml(python_dict, outfile_path):
+    raise NotImplementedError
+
+def dump_dict_to_yaml_temp():
+    raise NotImplementedError
+
+def make_temp_dir():
+    raise NotImplementedError
