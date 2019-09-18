45f1e504d52d8dc45be3bc85cb1976c7faa47826
Karl Sebby <karl.sebby@xdbio.com>
2019-09-18 08:42:07 -0600

    Removed utils directory. Test passing using xd_cwl_utils.validate.validate_repo().

--- tests/suite.py
+++ tests/suite.py
@@ -3,52 +3,23 @@ import logging
 import tempfile
 import os
 from unittest import defaultTestLoader
-from tests.test_tool_metadata import TestMakeToolMetadata, TestMakeParentToolMetadata, TestMakeSubtoolMetadata, TestAddTools
-from tests.test_script_metadata import TestScriptMetadata
-from tests.test_content_maps import TestToolMaps
-from tests.test_workflow_metadata import TestWorkflowMetadata
-from tests.test_get_metadata import TestMetadataFromBioTools
-from tests.test_validate import TestValidateMetadata
-from tests.test_validate_all_metadata_in_maps import TestValidateContent
-from tests.test_validate_tool_inputs import TestValidateInputs
+from xd_cwl_utils.validate import validate_repo
+
+class TestValidateRepo(unittest.TestCase):
+
+    def test_validate_repo(self):
+        validate_repo()
 
 
-def full_suite():
-    # Tool metadata classes
-    suite = defaultTestLoader.loadTestsFromTestCase(TestMakeToolMetadata)
-    suite.addTest(defaultTestLoader.loadTestsFromTestCase(TestMakeSubtoolMetadata))
-    suite.addTest(defaultTestLoader.loadTestsFromTestCase(TestMakeParentToolMetadata))
-    suite.addTest(defaultTestLoader.loadTestsFromTestCase(TestAddTools))
-
-    # Script metadata classes
-    suite.addTest((defaultTestLoader.loadTestsFromTestCase(TestScriptMetadata)))
-
-    # Workflow metadata classes
-    suite.addTest((defaultTestLoader.loadTestsFromTestCase(TestWorkflowMetadata)))
-
-    # Get metadata
-    suite.addTest(defaultTestLoader.loadTestsFromTestCase(TestMetadataFromBioTools))
-
-    # Validate
-    suite.addTest(defaultTestLoader.loadTestsFromTestCase(TestValidateMetadata))
-    suite.addTest(defaultTestLoader.loadTestsFromTestCase(TestValidateContent))
-
-    # Validate inputs
-    suite.addTest(defaultTestLoader.loadTestsFromTestCase(TestValidateInputs))
-
-    suite.addTest(defaultTestLoader.loadTestsFromTestCase(TestToolMaps))
-
+def test_suite():
+    suite = defaultTestLoader.loadTestsFromTestCase(TestValidateRepo)
     return suite
-
 
 
 def main():
     logging.basicConfig(level=logging.CRITICAL)
-    # Set temp directory
-    test_temp_dir = tempfile.TemporaryDirectory(prefix='cwlTest_')
-    os.environ['TEST_TMP_DIR'] = test_temp_dir.name
     os.environ['CONFIG_KEY'] = 'DEFAULT'
-    suite = full_suite()
+    suite = test_suite()
     unittest.TextTestRunner().run(suite)
     return
 
--- utilities/config.py
+++
@@ -1,8 +0,0 @@
-
-from pathlib import Path
-
-BASE_DIR = Path.cwd()
-CWL_TOOL_DIR = BASE_DIR / 'cwl-tools'
-CWL_SCRIPT_DIR = BASE_DIR / 'cwl-scripts'
-CWL_WORKFLOWS_DIR = BASE_DIR / 'cwl-workflows'
-CONTENT_MAPS_DIR = BASE_DIR / 'content_maps'
